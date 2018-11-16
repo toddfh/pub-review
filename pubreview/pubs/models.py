e7700b1ad87e00439f834ca6aa0449d9ff60195f
ToddFH <appalachee.red@gmail.com>
2018-11-16 15:30:31 -0500

    Changed pubs.models Pubs.description field to TextField

--- pubreview/pubs/models.py
+++ pubreview/pubs/models.py
@@ -7,7 +7,7 @@ class Pubs(models.Model):
     name = models.CharField(max_length=100, blank=False)
     address = models.CharField(max_length=100, blank=False)
     postal_code = models.CharField(max_length=10)
-    description = models.CharField(max_length=10)
+    description = models.TextField(blank=True, null=True)
     website = models.URLField()
     slug = models.SlugField(null=True, blank=True)
     pub_image = models.ImageField(upload_to=None, blank=True, null=True)
