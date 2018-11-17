8dbdd896b289c91ca5805970c7d659bcccc6ea93
Pradeep Susarla <pradeep@pradeeps.org>
2018-11-17 02:01:16 +0000

    added colorlib template and added geoip to detect location

--- pubreview/home/views.py
+++ pubreview/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
+import requests
+import json
 
 def home(request):
-    return HttpResponse('Main Home Page')
+	send_url = "http://api.ipstack.com/check?access_key=be2a093998316de88c9b49fb6176fe5c"
+	geo_req = requests.get(send_url)
+	geo_json = json.loads(geo_req.text)
+	latitude = geo_json['latitude']
+	longitude = geo_json['longitude']
+	city = geo_json['city']
+	country_name = geo_json['country_name']
+	context = {"city":city,"country":country_name}
+	return render(request, 'home/index.html', context)
 
